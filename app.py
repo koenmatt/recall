f7bb9d4d4b7322d7e4db2bbebbc0bc98bb9f214c
Matt Koen <69600516+koenmatt@users.noreply.github.com>
2024-07-01 15:15:07 -0700

    auth

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-import pandas as pd
-print('hello world')
+from flask import Flask
+import os
+import requests
+app = Flask(__name__)
+from recallai import RecallAi
+
+headers = f"Authorization: Token {os.getenv('TOKEN')}"  # Set Recall.ai token - https://api.recall.ai/auth/login/?next=/dashboard/apikeys/
+
+
+
+
+
+@app.route("/", methods=['POST'])
+def hello_world():
+    ...
+
+
+
+app.run(host='127.0.0.1', port=3000) 
